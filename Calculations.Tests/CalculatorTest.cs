6687ecb7324affa7c7f3c47bf17b20aebd4b966a
David Etzel <detzel@paytel.com>
2019-12-13 16:59:51 -0500

    initial commit

---
+++ Calculations.Tests/CalculatorTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        // Bad test method name
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        // Good test method name
+        public void Add_GivenTwoIntValues_ReturnsInt()
+        {
+            var calc = new Calculator(); //instance of the class with method to be tested
+            var result = calc.Add(1, 2); //execution of method for testing with input values
+            Assert.Equal(3, result); //comparing expectation (3) with actual result (result) 
+        }
+
+        [Fact]
+        public void AddDouble_GivenTwoDoubleValues_ReturnsDouble()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.23, 3.55);
+            Assert.Equal(4.7, result, 0); // compares exp vs act within "precision" (number of decimal places)
+        }
+
+        [Fact]
+        public void Add_GivenTwoIntValues_ReturnsSum()
+        {
+            var calc = new Calculator();
+            var result = calc.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void AddDouble_GivenDoubleIntValues_ReturnsSum()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.53, 1.45);
+            Assert.Equal(2.9, result, 0);
+        }
+    }
+}
---
+++ Calculations.Tests/NamesTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class NamesTest
+    {
+        [Fact]
+        public void MakeFullNameTest() // MakeFullName_GivenTwoStrings_ReturnsString()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("David", "Etzel");
+            Assert.Equal("david Etzel", result, ignoreCase:true); // compares exp vs act with strings of different letter cases
+            Assert.Contains("David", result); // checks that act value "contains" exp value
+            Assert.Contains("david", result, StringComparison.InvariantCultureIgnoreCase); // checks that act value "contains" exp value w/o regard to casing
+            Assert.StartsWith("David E", result); // checks based on starting characters
+            Assert.StartsWith("david E", result, StringComparison.InvariantCultureIgnoreCase); // checks based on starting characters w/o regard to casing
+            Assert.EndsWith("tzel", result); // checks based on ending characters
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", result); // checks based on regex
+        }
+
+        [Fact]
+        public void NickName_MustBeNull()
+        {
+            var names = new Names();
+            Assert.Null(names.NickName); // verifies a null value
+            names.NickName = "Dave";
+            Assert.NotNull(names.NickName); // verifies value is not null
+        }
+
+        [Fact]
+        public void MakeFullName_AlwaysReturnValue()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("David", "Etzel");
+            Assert.NotNull(result); // verifies the result of method is not null
+            Assert.True(!string.IsNullOrEmpty(result)); // verifies the result of method is a string that is not null
+            Assert.False(string.IsNullOrEmpty(result)); // same as above w/o double negative logic
+        }
+    }
+}
